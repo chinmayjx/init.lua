20237b5377441b9917fd44d15cfae95834f75a2e
Chinmay Jain <chinmay@capillarytech.com>
2024-07-22 09:41:06 +0530

    pytest class support

--- lua/cj/python.lua
+++ lua/cj/python.lua
@@ -5,9 +5,17 @@ local lastTest = nil
 local function pytestExecuteFn()
   vim.cmd.w()
   local file = vim.fn.expand("%")
-  local fn = CurrentFunction()
+  local fn, node = CurrentFunction({
+    nameValidator = function(name)
+      return name:sub(1, 4) == "test" or name:sub(-4) == "test"
+    end
+  })
+  local class, _ = ContainingClass({ node = node })
   if fn == nil then
     return
+  end
+  if class ~= nil then
+    fn = class .. "::" .. fn
   end
   lastTest = "python -m pytest -s " .. file .. "::" .. fn
   run.runInTerminal(lastTest)
--- lua/cj/ts_util.lua
+++ lua/cj/ts_util.lua
@@ -1,4 +1,13 @@
-function CurrentFunction()
+local function getNodeText(node)
+  -- TODO: handle multi-line nodes
+  local x1, y1, x2, y2 = node:range()
+  local line = vim.api.nvim_buf_get_lines(0, x1, x1 + 1, false)[1]:sub(y1 + 1, y2)
+  return line
+end
+
+function CurrentFunction(opts)
+  opts = opts or {}
+  local nameValidator = opts.nameValidator
   local match = {
     ["function_definition"] = true,
     ["function_declaration"] = true
@@ -9,11 +18,13 @@ function CurrentFunction()
   local findName = function(nd)
     local type = nd:type()
     if match[type] then
-      local name = nd:field("name")[1]
-      if name ~= nil then
-        local x1, y1, x2, y2 = name:range()
-        local line = vim.api.nvim_buf_get_lines(0, x1, x1 + 1, false)[1]:sub(y1 + 1, y2)
-        return line
+      local nameNode = nd:field("name")[1]
+      if nameNode ~= nil then
+        local name = getNodeText(nameNode)
+        if nameValidator and not nameValidator(name) then
+          return
+        end
+        return name
       end
     end
   end
@@ -21,20 +32,34 @@ function CurrentFunction()
   while node ~= nil do
     local name = findName(node)
     if name then
-      return name
+      return name, node
     end
     node = node:parent()
   end
   node = _node
+
   while node ~= nil do
     local name = findName(node)
     if name then
-      return name
+      return name, node
     end
     if node:next_sibling() == nil then
       node = node:parent()
     else
       node = node:next_sibling()
     end
+  end
+end
+
+function ContainingClass(opts)
+  opts = opts or {}
+  local node = opts.node or vim.treesitter.get_node()
+  while node ~= nil do
+    if node:type() == "class_definition" then
+      local nameNode = node:field("name")[1]
+      local name = getNodeText(nameNode)
+      return name, node
+    end
+    node = node:parent()
   end
 end
